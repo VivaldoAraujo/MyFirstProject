b0359fa093dfda7d202e446db7b3df1af1670bd9
VivaldoAraujo <vivaldo_juni_or@hotmail.com>
2023-06-28 21:50:02 +0100

    add

--- app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.meuprimeiroprojeto
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
